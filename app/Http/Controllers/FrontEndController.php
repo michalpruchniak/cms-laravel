4125dc87e84d76a894692e2b7d769cc28798a230
michal <mpruchniak@gmail.com>
2018-01-07 22:14:16 +0100

    frontend II

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -13,6 +13,12 @@ class FrontEndController extends Controller
         return view('welcome')
                 ->with('title', Setting::first()->site_name)
                 ->with('categories', Category::take(5)->get())
-                ->with('first_post', Post::orderBy('created_at', 'desc')->first());
+                ->with('first_post', Post::orderBy('created_at', 'desc')->first())
+                ->with('second_post', POST::orderBy('created_at', 'desc')->skip(1)->take(1)->get()->first())
+                ->with('third_post', POST::orderBy('created_at', 'desc')->skip(2)->take(1)->get()->first())
+                ->with('frontend', Category::find(7))
+                ->with('backend', Category::find(8))
+                ->with('wordpress', Category::find(9))
+                ->with('settings', Setting::first());
     }
 }
